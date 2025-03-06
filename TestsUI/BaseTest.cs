8fbaf0cc930c68729ba07c8ff681cfa5c723e222
Iryna Zybliyenka <Iryna_Zybliyenka@epam.com>
2025-03-06 16:35:35 +0300

    Added Page Objects for Selenium.

---
+++ TestsUI/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using Business.AppPageObjects;
+using Core.WebDriver;
+
+namespace TestsUI
+{
+    public abstract class BaseTest
+    {
+        protected readonly HomePage homePage = new HomePage();
+        protected readonly AboutPage aboutPage = new AboutPage();
+        protected readonly SearchPanel searchPanel = new SearchPanel();
+        protected readonly SearchPage searchPage = new SearchPage();
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            Browser.GetInstance();
+            Browser.MaximizeWindow();
+            Browser.NavigateTo();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            Browser.Close();
+        }
+    }
+}
