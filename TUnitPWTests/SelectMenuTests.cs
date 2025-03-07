ef56da0fa01f69146da70abfa9677e1c366fba68
PavelBadeha <pbadzekha@mail.ru>
2025-03-07 14:57:02 +0300

    Added TUnit project

---
+++ TUnitPWTests/SelectMenuTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using PWCore.WebDriver;
+using PWPages.DemoQA;
+
+namespace TUnitPWTests
+{  
+    public class SelectMenuTests
+    {
+        private Browser _browser;
+        private BrowserConfig _config;
+        private HomePage _homePage;
+
+        [Before(Test)]
+        public async Task Setup()
+        {
+            _config = BrowserConfig.Load();
+            _browser = new Browser(_config);
+            await _browser.InitializeAsync();
+            _homePage = new HomePage(_browser.GetPage(), _config.BaseUrl);
+            await _homePage.OpenAsync();
+        }
+
+        [Test]
+        public async Task SelectValueDropdownTest()
+        {
+            var widgetsPage = await _homePage.GoToWidgetsSectionAsync();
+            var selectMenuPage = await widgetsPage.GoToSelectMenuPageAsync();
+
+            string expectedOption = "A root option";
+            await selectMenuPage.SelectValueAsync(expectedOption);
+
+            string selectedOption = await selectMenuPage.GetSelectedValueAsync();
+            selectedOption.Should().Be(expectedOption);
+        }
+
+        [After(Test)]
+        public async Task Cleanup()
+        {
+            await _browser.Close();
+        }
+    }
+}
